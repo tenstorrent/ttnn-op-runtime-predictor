b187897f6ed1667f12e73b0ba0840ae1706acd0f
Xander Chin <xchin@tenstorrent.com>
2025-02-20 16:53:32 -0500

    compile string for macos

---
+++ mlpack/compile_mac.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+g++ -O3 -std=c++20 -DMLPACK_ENABLE_ANN_SERIALIZATION=ON -DDEBUG -o test_mlpregress test_mlpregress.cpp -L/opt/homebrew/lib -larmadillo -I/opt/homebrew/include -L/opt/homebrew/lib
