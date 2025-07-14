ba26b27234984c4efdaae87c5f2b50816cb45505
Guangyu Feng <gfeng@tenstorrent.com>
2025-07-14 18:28:41 +0000

    add mlpack as an external project

---
+++ install_dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+sudo apt-get install -y libarmadillo-dev libensmallen-dev libcereal-dev nlohmann-json3-dev
