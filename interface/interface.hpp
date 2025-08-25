9b8dd79cead9273e30b6699ce07523c78715b4d7
Brandon Rapanan <brapanan@tenstorrent.com>
2025-08-25 14:56:57 +0000

    Clang format applied.

--- interface/interface.hpp
+++ interface/interface.hpp
@@ -4,15 +4,12 @@
 
 #pragma once
 
-
+#include "../ops/include/ops.hpp"
+#include "interface_json_utils.hpp"
 #include <cstdint>
 #include <optional>
 #include <stdexcept>
 #include <string>
-#include <vector>
-#include <unordered_map>
-#include "../ops/include/ops.hpp"
-#include "interface_json_utils.hpp"
 
 namespace op_perf {
 
@@ -20,18 +17,20 @@ template <typename... Args>
 uint64_t get_runtime_from_model(const std::string &op_name, Args &&...args) {
 
   try {
-    const nlohmann::json op_category_json = op_perf::load_op_category_map(std::string(OP_CATEGORY_PATH) + "op_categories.json");
+    const nlohmann::json op_category_json = op_perf::load_op_category_map(
+        std::string(OP_CATEGORY_PATH) + "op_categories.json");
     if (!op_category_json.contains(op_name)) {
       throw std::runtime_error("Error: Unknown op name: " + op_name);
     }
-    const std::string& category = op_category_json[op_name];
+    const std::string &category = op_category_json[op_name];
 
     if (category == "eltwise_unary") {
-      return predict_eltwise_unary_runtime(op_name, std::forward<Args>(args)...);
+      return predict_eltwise_unary_runtime(op_name,
+                                           std::forward<Args>(args)...);
     } else {
       return 0;
     }
-  } catch (const std::runtime_error&) {
+  } catch (const std::exception &) {
     return 0;
   }
 }
--- interface/interface_json_utils.cpp
+++ interface/interface_json_utils.cpp
@@ -3,13 +3,13 @@
 namespace op_perf {
 
 nlohmann::json load_op_category_map(const std::string &json_path) {
-    std::ifstream infile(json_path);
-    if (!infile) {
-        throw std::runtime_error("Error: could not open file: " + json_path);
-    }
-    nlohmann::json op_map;
-    infile >> op_map;
-    return op_map;
+  std::ifstream infile(json_path);
+  if (!infile) {
+    throw std::runtime_error("Error: could not open file: " + json_path);
+  }
+  nlohmann::json op_map;
+  infile >> op_map;
+  return op_map;
 }
 
 } // namespace op_perf
