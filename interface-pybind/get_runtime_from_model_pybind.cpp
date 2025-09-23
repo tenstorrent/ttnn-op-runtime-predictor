f5a4134e3b582460165564603fd283a921bc0199
Brandon Rapanan <brapanan@tenstorrent.com>
2025-09-23 20:32:45 +0000

    Pybind11 build now working, using library pybind11_json for py::json -> nlohmann::json. Will clean up + possibly add pybinding tests?

---
+++ interface-pybind/get_runtime_from_model_pybind.cpp
@@ -0,0 +1,23 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include "pybind11_json/pybind11_json.hpp"
+
+#include "interface/interface.hpp"
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(ttnn_op_runtime_predictor, m) {
+    m.doc() = "Python bindings for the TTNN Op Runtime Predictor";
+
+    m.def("get_eltwise_unary_runtime_from_model", &op_perf::get_runtime_from_model<const nlohmann::json&, const nlohmann::json&>,
+          "Get the eltwise_unary runtime prediction from the model",
+          py::arg("op_name"), py::arg("tensor_json"), py::arg("optional_output_layout") = nlohmann::json::object());
+
+    m.def("get_concatenate_heads_runtime_from_model", &op_perf::get_runtime_from_model<const nlohmann::json&, const nlohmann::json&>,
+          "Get the concatenate_heads runtime prediction from the model",
+          py::arg("op_name"), py::arg("tensor_json"), py::arg("optional_output_layout") = nlohmann::json::object());
+
+    m.def("get_create_qkv_heads_runtime_from_model", &op_perf::get_runtime_from_model<const nlohmann::json&, const int&, const std::optional<int>&, const bool&>,
+          "Get the create_qkv_heads runtime prediction from the model",
+          py::arg("op_name"), py::arg("tensor_json"), py::arg("num_heads"), py::arg("num_kv_heads") = std::nullopt, py::arg("transpose_k_heads") = false);
+}
