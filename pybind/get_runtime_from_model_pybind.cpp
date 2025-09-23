d5c5d00ab3a9502d3487628f25ec75a998f61bff
Brandon Rapanan <brapanan@tenstorrent.com>
2025-09-23 17:25:26 +0000

    Include pybind11 as cpm module, create new dir pybind to contain pybind cpp code.

---
+++ pybind/get_runtime_from_model_pybind.cpp
@@ -0,0 +1,9 @@
+#include <pybind11/pybind11.h>
+#include <nlohmann/json.hpp>
+
+#include "interface/interface.hpp"
+
+// PYBIND11_MODULE(get_runtime_from_model, m) {
+//     //to be completed
+//     m.def("get_runtime_from_model", &op_perf::get_runtime_from_model);
+// }
