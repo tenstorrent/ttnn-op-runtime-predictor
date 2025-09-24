29c037b774633eabaed8cd85439bf9b56931010f
Brandon Rapanan <brapanan@tenstorrent.com>
2025-09-24 16:36:41 +0000

    Updated python bindings to have unique functions per mlp, e.g. get_exp_runtime().

--- interface-pybind/get_runtime_from_model_pybind.cpp
+++ interface-pybind/get_runtime_from_model_pybind.cpp
@@ -9,15 +9,15 @@ namespace py = pybind11;
 PYBIND11_MODULE(ttnn_op_runtime_predictor, m) {
     m.doc() = "Python bindings for the TTNN Op Runtime Predictor";
 
-    m.def("get_eltwise_unary_runtime_from_model", &op_perf::get_runtime_from_model<const nlohmann::json&, const nlohmann::json&>,
-          "Get the eltwise_unary runtime prediction from the model",
-          py::arg("op_name"), py::arg("tensor_json"), py::arg("optional_output_layout") = nlohmann::json::object());
+    m.def("get_exp_runtime", &op_perf::get_runtime_from_model<const nlohmann::json&, const nlohmann::json&>,
+          "Get the exp runtime prediction from the model",
+          py::arg("op_name") = "exp", py::arg("tensor_json"), py::arg("output_memory_config") = nlohmann::json::object());
 
-    m.def("get_concatenate_heads_runtime_from_model", &op_perf::get_runtime_from_model<const nlohmann::json&, const nlohmann::json&>,
+      m.def("get_concatenate_heads_runtime", &op_perf::get_runtime_from_model<const nlohmann::json&, const nlohmann::json&>,
           "Get the concatenate_heads runtime prediction from the model",
-          py::arg("op_name"), py::arg("tensor_json"), py::arg("optional_output_layout") = nlohmann::json::object());
+          py::arg("op_name") = "concatenate_heads", py::arg("tensor_json"), py::arg("output_memory_config") = nlohmann::json::object());
 
-    m.def("get_create_qkv_heads_runtime_from_model", &op_perf::get_runtime_from_model<const nlohmann::json&, const int&, const std::optional<int>&, const bool&>,
+    m.def("get_create_qkv_heads_runtime", &op_perf::get_runtime_from_model<const nlohmann::json&, const int&, const std::optional<int>&, const bool&>,
           "Get the create_qkv_heads runtime prediction from the model",
-          py::arg("op_name"), py::arg("tensor_json"), py::arg("num_heads"), py::arg("num_kv_heads") = std::nullopt, py::arg("transpose_k_heads") = false);
+          py::arg("op_name") = "create_qkv_heads", py::arg("tensor_json"), py::arg("num_heads"), py::arg("num_kv_heads") = std::nullopt, py::arg("transpose_k_heads") = false);
 }
