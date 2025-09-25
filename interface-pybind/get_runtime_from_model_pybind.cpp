34b03d730d5695f7f89cf6fc1d6699b27a87db29
Brandon Rapanan <brapanan@tenstorrent.com>
2025-09-25 20:31:28 +0000

    Added spdx license headers to new files.

--- interface-pybind/get_runtime_from_model_pybind.cpp
+++ interface-pybind/get_runtime_from_model_pybind.cpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
 #include "pybind11_json/pybind11_json.hpp"
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
