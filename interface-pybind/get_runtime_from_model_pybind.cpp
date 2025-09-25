c72c036021d5ee4816cd99c307706460605c931b
Brandon Rapanan <brapanan@tenstorrent.com>
2025-09-25 18:49:48 +0000

    Updated pyinds to create a single (with overloads) function get_runtime_from_model() which retains the model dispatching capabilities. This will require an overloaded python function for each different template instantiation of get_runtime_from_model() in interface.hpp.

--- interface-pybind/get_runtime_from_model_pybind.cpp
+++ interface-pybind/get_runtime_from_model_pybind.cpp
@@ -1,23 +1,38 @@
+#include "pybind11_json/pybind11_json.hpp"
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
-#include "pybind11_json/pybind11_json.hpp"
 
 #include "interface/interface.hpp"
 
 namespace py = pybind11;
 
+// python module ttnn_op_runtime_predictor. This python module exposes the
+// function get_runtime_from_model (from interface/interface.hpp) to python.
+// There are overloads for each template instatiation of get_runtime_from_model.
+// This is to allow the pybinded function to retain the built-in dispatch
+// capability of the C++ function.
+
 PYBIND11_MODULE(ttnn_op_runtime_predictor, m) {
-    m.doc() = "Python bindings for the TTNN Op Runtime Predictor";
+  m.doc() = "Python bindings for the TTNN Op Runtime Predictor";
 
-    m.def("get_exp_runtime", &op_perf::get_runtime_from_model<const nlohmann::json&, const nlohmann::json&>,
-          "Get the exp runtime prediction from the model",
-          py::arg("op_name") = "exp", py::arg("tensor_json"), py::arg("output_memory_config") = nlohmann::json::object());
+  m.def("get_runtime_from_model",
+        &op_perf::get_runtime_from_model<const nlohmann::json &,
+                                         const nlohmann::json &>,
+        "Query a model for the runtime prediction of an operation."
+        "This overloaded function can be used for any ttnn eltwise unary op "
+        "which has a trained model on the repo, or "
+        "ttnn.transformer.concatenate_heads op.",
+        py::arg("op_name"), py::arg("tensor_json"),
+        py::arg("output_memory_config") = nlohmann::json::object());
 
-      m.def("get_concatenate_heads_runtime", &op_perf::get_runtime_from_model<const nlohmann::json&, const nlohmann::json&>,
-          "Get the concatenate_heads runtime prediction from the model",
-          py::arg("op_name") = "concatenate_heads", py::arg("tensor_json"), py::arg("output_memory_config") = nlohmann::json::object());
-
-    m.def("get_create_qkv_heads_runtime", &op_perf::get_runtime_from_model<const nlohmann::json&, const int&, const std::optional<int>&, const bool&>,
-          "Get the create_qkv_heads runtime prediction from the model",
-          py::arg("op_name") = "create_qkv_heads", py::arg("tensor_json"), py::arg("num_heads"), py::arg("num_kv_heads") = std::nullopt, py::arg("transpose_k_heads") = false);
+  m.def("get_runtime_from_model",
+        &op_perf::get_runtime_from_model<const nlohmann::json &, const int &,
+                                         const std::optional<int> &,
+                                         const bool &>,
+        "Query a model for the runtime prediction of an operation."
+        "This overloaded function can be used for "
+        "ttnn.experimental.create_qkv_heads op.",
+        py::arg("op_name") = "create_qkv_heads", py::arg("tensor_json"),
+        py::arg("num_heads"), py::arg("num_kv_heads") = std::nullopt,
+        py::arg("transpose_k_heads") = false);
 }
